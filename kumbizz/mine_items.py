778983af42e20cb5d9d59fc58430edc175050b24
kamran-abedi <abedikamran84@gmail.com>
2025-06-03 19:34:54 +0330

    mine_items.py

--- kumbizz/mine_items.py
+++ kumbizz/mine_items.py
@@ -2,8 +2,8 @@ mine_drops = [
     {"name": "سنگ آهن", "chance": 55, "type": "common", "base_price": 200},
     {"name": "زغال سنگ", "chance": 30, "type": "common", "base_price": 250},
     {"name": "طلا", "chance": 8, "type": "rare", "base_price": 800},
-    {"name": "کریستال آبی", "chance": 5, "type": "rare", "base_price": 0},
-    {"name": "سنگ آتشین", "chance": 1.5, "type": "epic", "base_price": 0},
+    {"name": "کریستال آبی", "chance": 5, "type": "rare", "base_price": 2000},
+    {"name": "سنگ آتشین", "chance": 1.5, "type": "epic", "base_price": 3500},
     {"name": "الماس", "chance": 0.5, "type": "legendary", "base_price": 5000}
 ]
 mine_settings = {
