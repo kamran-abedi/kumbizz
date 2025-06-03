0c53a475c3a5b561577ebf643f06bc0f137b3541
kamran-abedi <abedikamran84@gmail.com>
2025-06-03 19:24:24 +0330

    farm_data.py

--- kumbizz/farm_data.py
+++ kumbizz/farm_data.py
@@ -2,21 +2,35 @@ farm_data = {
     "مرغ": {
         "product": "تخم مرغ",
         "interval_hours": 6,
-        "base_price": 2000
+        "price": 2000
     },
     "گاو": {
         "product": "شیر",
         "interval_hours": 12,
-        "base_price": 4000
+        "price": 4000
     },
     "زنبور": {
         "product": "عسل",
         "interval_hours": 24,
-        "base_price": 6000
+        "price": 6000
     },
     "مزرعه گیاه دارویی": {
         "product": "گیاه دارویی",
         "interval_hours": 12,
-        "base_price": 8000
+        "price": 8000
+    }
+}
+farm_sellable = {
+    "تخم مرغ": {
+        "base_price": 100
+    },
+    "تخم مرغ": {
+        "base_price": 180
+    },
+    "تخم مرغ": {
+        "base_price": 300
+    },
+    "تخم مرغ": {
+        "base_price": 500
     }
 }
