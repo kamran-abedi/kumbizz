cf00e071df0e11145d04f6a9e41d5d000068b054
kamran-abedi <abedikamran84@gmail.com>
2025-06-03 19:19:32 +0330

    mine_items.py

--- kumbizz/mine_items.py
+++ kumbizz/mine_items.py
@@ -1,10 +1,10 @@
 mine_drops = [
-    {"name": "سنگ آهن", "chance": 55, "type": "common", "sell_price": 200},
-    {"name": "زغال سنگ", "chance": 30, "type": "common", "sell_price": 250},
-    {"name": "طلا", "chance": 8, "type": "rare", "sell_price": 800},
-    {"name": "کریستال آبی", "chance": 5, "type": "rare", "sell_price": 0},
-    {"name": "سنگ آتشین", "chance": 1.5, "type": "epic", "sell_price": 0},
-    {"name": "الماس", "chance": 0.5, "type": "legendary", "sell_price": 5000}
+    {"name": "سنگ آهن", "chance": 55, "type": "common", "base_price": 200},
+    {"name": "زغال سنگ", "chance": 30, "type": "common", "base_price": 250},
+    {"name": "طلا", "chance": 8, "type": "rare", "base_price": 800},
+    {"name": "کریستال آبی", "chance": 5, "type": "rare", "base_price": 0},
+    {"name": "سنگ آتشین", "chance": 1.5, "type": "epic", "base_price": 0},
+    {"name": "الماس", "chance": 0.5, "type": "legendary", "base_price": 5000}
 ]
 mine_settings = {
     1: {"cooldown": 6, "count": 1},
