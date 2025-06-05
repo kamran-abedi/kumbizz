b9d6f3e7882ae544843b1331da3a115f4b92861c
kamran-abedi <abedikamran84@gmail.com>
2025-06-05 11:53:51 +0330

    farm_data.py

--- kumbizz/farm_data.py
+++ kumbizz/farm_data.py
@@ -18,11 +18,41 @@ farm_data = {
         "product": "گیاه دارویی",
         "interval_hours": 12,
         "price": 8000
+    },
+    "گوسفند": {
+        "product": "پشم",
+        "interval_hours": 4,
+        "price": 5000
+    },
+    "اردک": {
+        "product": "پر",
+        "interval_hours": 12,
+        "price": 10000
+    },
+    "مزرعه جو": {
+        "product": "جو",
+        "interval_hours": 8,
+        "price": 3000
+    },
+    "مزرعه ذرت": {
+        "product": "ذرت",
+        "interval_hours": 12,
+        "price": 8000
+    },
+    "مزرعه گندم": {
+        "product": "گندم",
+        "interval_hours": 3,
+        "price": 1500
     }
 }
 farm_sellable = [
     {"name": "تخم مرغ", "base_price": 100},
     {"name": "شیر", "base_price": 180},
     {"name": "عسل", "base_price": 300},
-    {"name": "گیاه دارویی", "base_price": 500}
+    {"name": "گیاه دارویی", "base_price": 500},
+    {"name": "پشم", "base_price": 200},
+    {"name": "پر", "base_price": 800},
+    {"name": "جو", "base_price": 150},
+    {"name": "ذرت", "base_price": 250},
+    {"name": "گندم", "base_price": 75}
 ]
