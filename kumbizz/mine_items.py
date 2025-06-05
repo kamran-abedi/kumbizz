c289c4b4b918de109256e0713ceac869ec203f0d
kamran-abedi <abedikamran84@gmail.com>
2025-06-05 11:54:23 +0330

    mine_items.py

--- kumbizz/mine_items.py
+++ kumbizz/mine_items.py
@@ -1,15 +1,73 @@
 mine_drops = [
-    {"name": "سنگ آهن", "chance": 55, "type": "common", "base_price": 200},
-    {"name": "زغال سنگ", "chance": 30, "type": "common", "base_price": 250},
-    {"name": "طلا", "chance": 8, "type": "rare", "base_price": 800},
-    {"name": "کریستال آبی", "chance": 5, "type": "rare", "base_price": 2000},
-    {"name": "سنگ آتشین", "chance": 1.5, "type": "epic", "base_price": 3500},
-    {"name": "الماس", "chance": 0.5, "type": "legendary", "base_price": 5000}
+    {"name": "سنگ",
+    "level_required": 1,
+    "chance": 40},
+    {"name": "رغال سنگ",
+    "level_required": 1,
+    "chance": 30},
+    {"name": "سنگ آهن",
+    "level_required": 2,
+    "chance": 25},
+    {"name": "سنگ مس",
+    "level_required": 3,
+    "chance": 20},
+    {"name": "نقره",
+    "level_required": 3,
+    "chance": 15},
+    {"name": "طلا",
+    "level_required": 4,
+    "chance": 8},
+    {"name": "کریستال آبی",
+    "level_required": 4,
+    "chance": 5},
+    {"name": "الماس",
+    "level_required": 5,
+    "chance": 3},
+    {"name": "سنگ افسانه ای",
+    "level_required": 5,
+    "chance": 2},
+    {"name": "اورانیوم",
+    "level_required": 6,
+    "chance": 2},
+    {"name": "شهاب سنگ",
+    "level_required": 6,
+    "chance": 1}
 ]
-mine_settings = {
-    1: {"cooldown": 6, "count": 1},
-    2: {"cooldown": 6, "count": 2},
-    3: {"cooldown": 4, "count": 2},
-    4: {"cooldown": 4, "count": 3},
-    5: {"cooldown": 3, "count": 3}
+mine_data = {
+    1: {
+        "cooldown": 6,
+        "count": 1,
+        "unlocks": ["سنگ", "زغال سنگ"],
+        "level_price": 15000
+    },
+    2: {
+        "cooldown": 5,
+        "count": 1,
+        "unlocks": ["سنگ آهن"],
+        "level_price": 25000
+    },
+    3: {
+        "cooldown": 5,
+        "count": 2,
+        "unlocks": ["سنگ مس", "نقره"],
+        "level_price": 60000
+    },
+    4: {
+        "cooldown": 4,
+        "count": 2,
+        "unlocks": ["طلا", "کریستال آبی"],
+        "level_price": 150000
+    },
+    5: {
+        "cooldown": 4,
+        "count": 3,
+        "unlocks": ["الماس", "سنگ افسانه ای"],
+        "level_price": 350000
+    },
+    6: {
+        "cooldown": 3,
+        "count": 4,
+        "unlocks": ["اورانیوم", "شهاب سنگ"],
+        "level_price": 750000
+    }
 }
