399323903400a3f709c8efe620213cc22ef575fe
kamran-abedi <abedikamran84@gmail.com>
2025-06-06 17:56:42 +0330

    farm_data.py

--- kumbizz/farm_data.py
+++ kumbizz/farm_data.py
@@ -1,48 +1,57 @@
 farm_data = {
     "مرغ": {
         "product": "تخم مرغ",
-        "interval_hours": 6,
-        "price": 2000
+        "interval_hours": 4,
+        "price": 2000,
+        "product_price": 100
     },
     "گاو": {
         "product": "شیر",
-        "interval_hours": 12,
-        "price": 4000
+        "interval_hours": 6,
+        "price": 4000,
+        "product_price": 180
     },
     "زنبور": {
         "product": "عسل",
-        "interval_hours": 24,
-        "price": 6000
+        "interval_hours": 8,
+        "price": 6000,
+        "product_price": 300
     },
     "مزرعه گیاه دارویی": {
         "product": "گیاه دارویی",
         "interval_hours": 12,
-        "price": 8000
+        "price": 8000,
+        "product_price": 500
     },
     "گوسفند": {
         "product": "پشم",
         "interval_hours": 4,
-        "price": 5000
+        "price": 5000,
+        "product_price": 200
     },
     "اردک": {
         "product": "پر",
         "interval_hours": 12,
-        "price": 10000
+        "price": 10000,
+        "product_price": 800
     },
     "مزرعه جو": {
         "product": "جو",
-        "interval_hours": 8,
-        "price": 3000
+        "interval_hours": 6,
+        "price": 3000,
+        "product_price": 150
     },
     "مزرعه ذرت": {
         "product": "ذرت",
         "interval_hours": 12,
-        "price": 8000
+        "price": 8000,
+        "product_price": 250
     },
     "مزرعه گندم": {
         "product": "گندم",
         "interval_hours": 3,
-        "price": 1500
+        "price": 1500,
+        "product_price": 75
     }
 }
 farm_sellable = [
