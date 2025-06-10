310fb00290589bfc61f8326a8ad41be7eb423a82
kamran-abedi <abedikamran84@gmail.com>
2025-06-10 09:53:39 +0330

    business_data.py

---
+++ kumbizz/business_data.py
@@ -0,0 +1,77 @@
+business_data = {
+  "آسیاب": {
+    "base_input": {"گندم": 1},
+    "base_output": {"آرد": 4},
+    "upgrade_cost": 20000
+  },
+  "تولیدی نان": {
+    "base_input": {"آرد": 1},
+    "base_output": {"نان": 3},
+    "upgrade_cost": 20000
+  },
+  "شیرینی‌پزی": {
+    "base_input": {"نان": 1, "شیر": 1},
+    "base_output": {"کیک": 2},
+    "upgrade_cost": 25000
+  },
+  "تولیدی لباس": {
+    "base_input": {"پشم": 1, "گیاه دارویی": 1},
+    "base_output": {"لباس سنتی": 2},
+    "upgrade_cost": 25000
+  },
+  "شمع‌سازی": {
+    "base_input": {"موم": 1, "گیاه دارویی": 1},
+    "base_output": {"شمع طبی": 2},
+    "upgrade_cost": 25000
+  },
+  "کارگاه پنیر": {
+    "base_input": {"شیر": 1},
+    "base_output": {"پنیر": 3},
+    "upgrade_cost": 20000
+  },
+  "نانوایی سنتی": {
+    "base_input": {"آرد": 1, "پنیر": 1},
+    "base_output": {"نون و پنیر": 2},
+    "upgrade_cost": 30000
+  },
+  "کارگاه بالشت‌دوزی": {
+    "base_input": {"پشم": 1, "پر": 1},
+    "base_output": {"بالشت": 2},
+    "upgrade_cost": 30000
+  },
+  "تولیدی تیتاپ": {
+    "base_input": {"کیک": 1, "عسل": 1},
+    "base_output": {"تیتاپ": 2},
+    "upgrade_cost": 30000
+  },
+  "آجرپزی": {
+    "base_input": {"سنگ": 1, "رغال سنگ": 1},
+    "base_output": {"آجر": 2},
+    "upgrade_cost": 25000
+  },
+  "کارگاه طلاسازی": {
+    "base_input": {"طلا": 1},
+    "base_output": {"شمش طلا": 2},
+    "upgrade_cost": 50000
+  },
+  "شیشه‌گری کریستال": {
+    "base_input": {"کریستال آبی": 1, "سنگ": 1},
+    "base_output": {"شیشه کریستال": 1},
+    "upgrade_cost": 60000
+  },
+  "پاپ کورن‌سازی": {
+    "base_input": {"ذرت": 1},
+    "base_output": {"پاپ کورن": 2},
+    "upgrade_cost": 20000
+  },
+  "تولیدی کره": {
+    "base_input": {"شیر": 2},
+    "base_output": {"کره": 2},
+    "upgrade_cost": 25000
+  },
+  "زره‌سازی": {
+    "base_input": {"آهن": 1, "پشم": 1},
+    "base_output": {"زره آهنین": 2},
+    "upgrade_cost": 35000
+  }
+}
