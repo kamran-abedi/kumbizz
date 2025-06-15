d449a7d1031fda1141d573b6910faa0c89199cbc
kamran-abedi <abedikamran84@gmail.com>
2025-06-15 16:00:22 +0330

    business_data.py

--- kumbizz/business_data.py
+++ kumbizz/business_data.py
@@ -1,67 +1,67 @@
 business_data = {
   "آسیاب": {
     "base_input": {"گندم": 1},
-    "base_output": {"آرد": 4},
-    "upgrade_cost": 20000,
+    "base_output": {"آرد": 2},
+    "upgrade_cost": 50000,
     "cooldown": 900_000
   },
   "تولیدی نان": {
     "base_input": {"آرد": 1},
-    "base_output": {"نان": 3},
-    "upgrade_cost": 20000,
+    "base_output": {"نان": 1},
+    "upgrade_cost": 60000,
     "cooldown": 1800_000
   },
   "شیرینی پزی": {
-    "base_input": {"آرد": 2},
-    "base_output": {"کیک": 3},
-    "upgrade_cost": 25000,
+    "base_input": {"آرد": 3},
+    "base_output": {"کیک": 2},
+    "upgrade_cost": 75000,
     "cooldown": 3600_000
   },
   "کارگاه لباس محلی": {
     "base_input": {"پشم": 1, "گیاه دارویی": 1},
-    "base_output": {"لباس سنتی": 2},
-    "upgrade_cost": 25000,
+    "base_output": {"لباس سنتی": 1},
+    "upgrade_cost": 65000,
     "cooldown": 3600_000
   },
   "شمع سازی": {
     "base_input": {"عسل": 1, "گیاه دارویی": 1},
-    "base_output": {"شمع طبی": 2},
-    "upgrade_cost": 25000,
+    "base_output": {"شمع طبی": 1},
+    "upgrade_cost": 75000,
     "cooldown": 1800_000
   },
   "کارگاه پنیر": {
     "base_input": {"شیر": 1},
-    "base_output": {"پنیر": 3},
-    "upgrade_cost": 20000,
+    "base_output": {"پنیر": 2},
+    "upgrade_cost": 55000,
     "cooldown": 900_000
   },
   "تولیدی صبحانه": {
     "base_input": {"نان": 1, "پنیر": 1},
-    "base_output": {"نون و پنیر": 2},
-    "upgrade_cost": 30000,
+    "base_output": {"نون و پنیر": 1},
+    "upgrade_cost": 80000,
     "cooldown": 3600_000
   },
   "کارگاه بالشت دوزی": {
     "base_input": {"پر": 1},
-    "base_output": {"بالشت": 2},
-    "upgrade_cost": 30000,
+    "base_output": {"بالشت": 1},
+    "upgrade_cost": 75000,
     "cooldown": 1800_000
   },
   "تولیدی تیتاپ": {
     "base_input": {"کیک": 1, "عسل": 1},
-    "base_output": {"تیتاپ": 2},
-    "upgrade_cost": 30000,
+    "base_output": {"تیتاپ": 1},
+    "upgrade_cost": 90000,
     "cooldown": 3600_000
   },
   "آجرپزی": {
     "base_input": {"سنگ": 1},
-    "base_output": {"آجر": 4},
-    "upgrade_cost": 25000,
+    "base_output": {"آجر": 2},
+    "upgrade_cost": 65000,
     "cooldown": 900_000
   },
   "کارگاه طلاسازی": {
     "base_input": {"طلا": 1},
-    "base_output": {"شمش طلا": 2},
+    "base_output": {"شمش طلا": 1},
     "upgrade_cost": 50000,
     "cooldown": 3600_000
   },
@@ -73,38 +73,38 @@ business_data = {
   },
   "پاپ کورن سازی": {
     "base_input": {"ذرت": 1},
-    "base_output": {"پاپ کورن": 5},
-    "upgrade_cost": 20000,
+    "base_output": {"پاپ کورن": 2},
+    "upgrade_cost": 50000,
     "cooldown": 900_000
   },
   "تولیدی کره": {
-    "base_input": {"شیر": 2},
-    "base_output": {"کره": 3},
-    "upgrade_cost": 25000,
+    "base_input": {"شیر": 3},
+    "base_output": {"کره": 2},
+    "upgrade_cost": 70000,
     "cooldown": 900_000
   },
   "زره سازی": {
     "base_input": {"آهن": 1, "پشم": 1},
-    "base_output": {"زره آهنین": 2},
-    "upgrade_cost": 35000,
+    "base_output": {"زره آهنین": 1},
+    "upgrade_cost": 60000,
     "cooldown": 1800_000
   },
   "تولیدی دلستر": {
     "base_input": {"جو": 1},
-    "base_output": {"آبجو": 4},
-    "upgrade_cost": 20000,
+    "base_output": {"آبجو": 2},
+    "upgrade_cost": 50000,
     "cooldown": 900_000
   },
   "صبحانه فروشی": {
     "base_input": {"تخم مرغ": 1},
-    "base_output": {"نیمرو": 3},
-    "upgrade_cost": 20000,
+    "base_output": {"نیمرو": 1},
+    "upgrade_cost": 60000,
     "cooldown": 900_000
   },
   "تولیدی پوشاک": {
     "base_input": {"پشم": 1},
-    "base_output": {"لباس": 3},
-    "upgrade_cost": 20000,
+    "base_output": {"لباس": 1},
+    "upgrade_cost": 55000,
     "cooldown": 900_000
   }
 }
